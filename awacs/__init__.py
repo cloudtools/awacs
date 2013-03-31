c67a8d0943a7e5e2258ab439b82a5592ed66feb1
Mark Peek <mark@peek.org>
2013-03-31 12:12:48 -0700

    Bump to version 0.2.0

--- awacs/__init__.py
+++ awacs/__init__.py
@@ -8,7 +8,7 @@ import json
 import re
 import types
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 valid_names = re.compile(r'^[a-zA-Z0-9]+$')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='awacs',
-    version='0.1.0',
+    version='0.2.0',
     description="AWS Access Policy Language creation library",
     author="Mark Peek",
     author_email="mark@peek.org",
