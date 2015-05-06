9277ae901bbd41fc38877ddd9b83cc7ed1c1725e
Michael Barrett <loki77@gmail.com>
2015-05-06 14:31:04 -0700

    Add many new actions
    - Also adds ARN & Action types

--- awacs/route53.py
+++ awacs/route53.py
@@ -3,15 +3,53 @@
 #
 # See LICENSE file for full license.
 
-from aws import Action
+from aws import Action, BaseARN
 
 service_name = 'Amazon Route 53'
 prefix = 'route53'
 
-ChangeResourceRecordSets = Action(prefix, 'ChangeResourceRecordSets')
-CreateHostedZone = Action(prefix, 'CreateHostedZone')
-DeleteHostedZone = Action(prefix, 'DeleteHostedZone')
-GetChange = Action(prefix, 'GetChange')
-GetHostedZone = Action(prefix, 'GetHostedZone')
-ListHostedZones = Action(prefix, 'ListHostedZones')
-ListResourceRecordSets = Action(prefix, 'ListResourceRecordSets')
+
+class ARN(BaseARN):
+    def __init__(self, resource="*"):
+        sup = super(ARN, self)
+        sup.__init__(service=prefix, resource=resource)
+
+
+class Route53Action(Action):
+    def __init__(self, action=None):
+        self.prefix = prefix
+        self.action = action
+
+
+AssociateVPCWithHostedZone = Route53Action("AssociateVPCWithHostedZone")
+ChangeResourceRecordSets = Route53Action("ChangeResourceRecordSets")
+ChangeTagsForResource = Route53Action("ChangeTagsForResource")
+CreateHealthCheck = Route53Action("CreateHealthCheck")
+CreateHostedZone = Route53Action("CreateHostedZone")
+CreateReusableDelegationSet = Route53Action("CreateReusableDelegationSet")
+DeleteHealthCheck = Route53Action("DeleteHealthCheck")
+DeleteHostedZone = Route53Action("DeleteHostedZone")
+DeleteReusableDelegationSet = Route53Action("DeleteReusableDelegationSet")
+DisassociateVPCFromHostedZone = Route53Action("DisassociateVPCFromHostedZone")
+GetChange = Route53Action("GetChange")
+GetCheckerIpRanges = Route53Action("GetCheckerIpRanges")
+GetGeoLocation = Route53Action("GetGeoLocation")
+GetHealthCheck = Route53Action("GetHealthCheck")
+GetHealthCheckCount = Route53Action("GetHealthCheckCount")
+GetHealthCheckLastFailureReason = \
+    Route53Action("GetHealthCheckLastFailureReason")
+GetHealthCheckStatus = Route53Action("GetHealthCheckStatus")
+GetHostedZone = Route53Action("GetHostedZone")
+GetHostedZoneCount = Route53Action("GetHostedZoneCount")
+GetReusableDelegationSet = Route53Action("GetReusableDelegationSet")
+ListGeoLocations = Route53Action("ListGeoLocations")
+ListHealthChecks = Route53Action("ListHealthChecks")
+ListHostedZones = Route53Action("ListHostedZones")
+ListHostedZonesByName = Route53Action("ListHostedZonesByName")
+ListOperations = Route53Action("ListOperations")
+ListResourceRecordSets = Route53Action("ListResourceRecordSets")
+ListReusableDelegationSets = Route53Action("ListReusableDelegationSets")
+ListTagsForResource = Route53Action("ListTagsForResource")
+ListTagsForResources = Route53Action("ListTagsForResources")
+UpdateHealthCheck = Route53Action("UpdateHealthCheck")
+UpdateHostedZoneComment = Route53Action("UpdateHostedZoneComment")
--- awacs/route53domains.py
+++ awacs/route53domains.py
@@ -8,18 +8,28 @@ from aws import Action
 service_name = 'Amazon Route53 Domains'
 prefix = 'route53domains'
 
-CheckDomainAvailability = Action(prefix, 'CheckDomainAvailability')
-DeleteDomain = Action(prefix, 'DeleteDomain')
-DisableDomainTransferLock = Action(prefix, 'DisableDomainTransferLock')
-EnableDomainTransferLock = Action(prefix, 'EnableDomainTransferLock')
-GetDomainDetail = Action(prefix, 'GetDomainDetail')
-GetOperationDetail = Action(prefix, 'GetOperationDetail')
-ListDomains = Action(prefix, 'ListDomains')
-ListOperations = Action(prefix, 'ListOperations')
-RegisterDomain = Action(prefix, 'RegisterDomain')
-RetrieveDomainAuthCode = Action(prefix, 'RetrieveDomainAuthCode')
-TransferDomain = Action(prefix, 'TransferDomain')
-UpdateDomainContact = Action(prefix, 'UpdateDomainContact')
-UpdateDomainContactPrivacy = \
-    Action(prefix, 'UpdateDomainContactPrivacy')
-UpdateDomainNameservers = Action(prefix, 'UpdateDomainNameservers')
+
+class Route53DomainsAction(Action):
+    def __init__(self, action=None):
+        self.prefix = prefix
+        self.action = action
+
+
+CheckDomainAvailability = Route53DomainsAction("CheckDomainAvailability")
+DeleteTagsForDomain = Route53DomainsAction("DeleteTagsForDomain")
+DisableDomainAutoRenew = Route53DomainsAction("DisableDomainAutoRenew")
+DisableDomainTransferLock = Route53DomainsAction("DisableDomainTransferLock")
+EnableDomainAutoRenew = Route53DomainsAction("EnableDomainAutoRenew")
+EnableDomainTransferLock = Route53DomainsAction("EnableDomainTransferLock")
+GetDomainDetail = Route53DomainsAction("GetDomainDetail")
+GetOperationDetail = Route53DomainsAction("GetOperationDetail")
+ListDomains = Route53DomainsAction("ListDomains")
+ListOperations = Route53DomainsAction("ListOperations")
+ListTagsForDomain = Route53DomainsAction("ListTagsForDomain")
+RegisterDomain = Route53DomainsAction("RegisterDomain")
+RetrieveDomainAuthCode = Route53DomainsAction("RetrieveDomainAuthCode")
+TransferDomain = Route53DomainsAction("TransferDomain")
+UpdateDomainContact = Route53DomainsAction("UpdateDomainContact")
+UpdateDomainContactPrivacy = Route53DomainsAction("UpdateDomainContactPrivacy")
+UpdateDomainNameservers = Route53DomainsAction("UpdateDomainNameservers")
+UpdateTagsForDomains = Route53DomainsAction("UpdateTagsForDomains")
