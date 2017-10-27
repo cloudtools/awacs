f5bd04b428efed90bbe2a400df35b868fe476928
Marcel Prince <mprince@users.noreply.github.com>
2017-10-26 21:30:29 -0700

    Update actions as of 10-26-17

---
+++ awacs/glue.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2012-2013, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from aws import Action as BaseAction
+from aws import BaseARN
+
+service_name = 'AWS Glue'
+prefix = 'glue'
+
+
+class Action(BaseAction):
+    def __init__(self, action=None):
+        sup = super(Action, self)
+        sup.__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource='', region='', account=''):
+        sup = super(ARN, self)
+        sup.__init__(service=prefix, resource=resource, region=region,
+                     account=account)
+
+
+BatchCreatePartition = Action('BatchCreatePartition')
+BatchDeleteConnection = Action('BatchDeleteConnection')
+BatchDeletePartition = Action('BatchDeletePartition')
+BatchDeleteTable = Action('BatchDeleteTable')
+BatchGetPartition = Action('BatchGetPartition')
+CreateClassifier = Action('CreateClassifier')
+CreateConnection = Action('CreateConnection')
+CreateCrawler = Action('CreateCrawler')
+CreateDatabase = Action('CreateDatabase')
+CreateDevEndpoint = Action('CreateDevEndpoint')
+CreateJob = Action('CreateJob')
+CreatePartition = Action('CreatePartition')
+CreateScript = Action('CreateScript')
+CreateTable = Action('CreateTable')
+CreateTrigger = Action('CreateTrigger')
+CreateUserDefinedFunction = Action('CreateUserDefinedFunction')
+DeleteClassifier = Action('DeleteClassifier')
+DeleteConnection = Action('DeleteConnection')
+DeleteCrawler = Action('DeleteCrawler')
+DeleteDatabase = Action('DeleteDatabase')
+DeleteDevEndpoint = Action('DeleteDevEndpoint')
+DeleteJob = Action('DeleteJob')
+DeletePartition = Action('DeletePartition')
+DeleteTable = Action('DeleteTable')
+DeleteTrigger = Action('DeleteTrigger')
+DeleteUserDefinedFunction = Action('DeleteUserDefinedFunction')
+GetCatalogImportStatus = Action('GetCatalogImportStatus')
+GetClassifier = Action('GetClassifier')
+GetClassifiers = Action('GetClassifiers')
+GetConnection = Action('GetConnection')
+GetConnections = Action('GetConnections')
+GetCrawler = Action('GetCrawler')
+GetCrawlerMetrics = Action('GetCrawlerMetrics')
+GetCrawlers = Action('GetCrawlers')
+GetDatabase = Action('GetDatabase')
+GetDatabases = Action('GetDatabases')
+GetDataflowGraph = Action('GetDataflowGraph')
+GetDevEndpoint = Action('GetDevEndpoint')
+GetDevEndpoints = Action('GetDevEndpoints')
+GetJob = Action('GetJob')
+GetJobRun = Action('GetJobRun')
+GetJobRuns = Action('GetJobRuns')
+GetJobs = Action('GetJobs')
+GetMapping = Action('GetMapping')
+GetPartition = Action('GetPartition')
+GetPartitions = Action('GetPartitions')
+GetPlan = Action('GetPlan')
+GetTable = Action('GetTable')
+GetTableVersions = Action('GetTableVersions')
+GetTables = Action('GetTables')
+GetTrigger = Action('GetTrigger')
+GetTriggers = Action('GetTriggers')
+GetUserDefinedFunction = Action('GetUserDefinedFunction')
+GetUserDefinedFunctions = Action('GetUserDefinedFunctions')
+ImportCatalogToGlue = Action('ImportCatalogToGlue')
+ResetJobBookmark = Action('ResetJobBookmark')
+StartCrawler = Action('StartCrawler')
+StartCrawlerSchedule = Action('StartCrawlerSchedule')
+StartJobRun = Action('StartJobRun')
+StartTrigger = Action('StartTrigger')
+StopCrawler = Action('StopCrawler')
+StopCrawlerSchedule = Action('StopCrawlerSchedule')
+StopTrigger = Action('StopTrigger')
+UpdateClassifier = Action('UpdateClassifier')
+UpdateConnection = Action('UpdateConnection')
+UpdateCrawler = Action('UpdateCrawler')
+UpdateDatabase = Action('UpdateDatabase')
+UpdateDevEndpoint = Action('UpdateDevEndpoint')
+UpdateJob = Action('UpdateJob')
+UpdatePartition = Action('UpdatePartition')
+UpdateTable = Action('UpdateTable')
+UpdateTrigger = Action('UpdateTrigger')
+UpdateUserDefinedFunction = Action('UpdateUserDefinedFunction')
---
+++ awacs/greengrass.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2012-2013, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from aws import Action as BaseAction
+from aws import BaseARN
+
+service_name = 'AWS Greengrass'
+prefix = 'greengrass'
+
+
+class Action(BaseAction):
+    def __init__(self, action=None):
+        sup = super(Action, self)
+        sup.__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource='', region='', account=''):
+        sup = super(ARN, self)
+        sup.__init__(service=prefix, resource=resource, region=region,
+                     account=account)
+
+
+AssociateRoleToGroup = Action('AssociateRoleToGroup')
+AssociateServiceRoleToAccount = Action('AssociateServiceRoleToAccount')
+CreateCoreDefinition = Action('CreateCoreDefinition')
+CreateCoreDefinitionVersion = Action('CreateCoreDefinitionVersion')
+CreateDeployment = Action('CreateDeployment')
+CreateDeviceDefinition = Action('CreateDeviceDefinition')
+CreateDeviceDefinitionVersion = Action('CreateDeviceDefinitionVersion')
+CreateFunctionDefinition = Action('CreateFunctionDefinition')
+CreateFunctionDefinitionVersion = \
+    Action('CreateFunctionDefinitionVersion')
+CreateGroup = Action('CreateGroup')
+CreateGroupCertificateAuthority = \
+    Action('CreateGroupCertificateAuthority')
+CreateGroupVersion = Action('CreateGroupVersion')
+CreateLoggerDefinition = Action('CreateLoggerDefinition')
+CreateLoggerDefinitionVersion = Action('CreateLoggerDefinitionVersion')
+CreateSubscriptionDefinition = Action('CreateSubscriptionDefinition')
+CreateSubscriptionDefinitionVersion = \
+    Action('CreateSubscriptionDefinitionVersion')
+DeleteCoreDefinition = Action('DeleteCoreDefinition')
+DeleteDeviceDefinition = Action('DeleteDeviceDefinition')
+DeleteFunctionDefinition = Action('DeleteFunctionDefinition')
+DeleteGroup = Action('DeleteGroup')
+DeleteLoggerDefinition = Action('DeleteLoggerDefinition')
+DeleteSubscriptionDefinition = Action('DeleteSubscriptionDefinition')
+DisassociateRoleFromGroup = Action('DisassociateRoleFromGroup')
+DisassociateServiceRoleFromAccount = \
+    Action('DisassociateServiceRoleFromAccount')
+GetAssociatedRole = Action('GetAssociatedRole')
+GetConnectivityInfo = Action('GetConnectivityInfo')
+GetCoreDefinition = Action('GetCoreDefinition')
+GetCoreDefinitionVersion = Action('GetCoreDefinitionVersion')
+GetDeploymentStatus = Action('GetDeploymentStatus')
+GetDeviceDefinition = Action('GetDeviceDefinition')
+GetDeviceDefinitionVersion = Action('GetDeviceDefinitionVersion')
+GetFunctionDefinition = Action('GetFunctionDefinition')
+GetFunctionDefinitionVersion = Action('GetFunctionDefinitionVersion')
+GetGroup = Action('GetGroup')
+GetGroupCertificateAuthority = Action('GetGroupCertificateAuthority')
+GetGroupCertificateConfiguration = \
+    Action('GetGroupCertificateConfiguration')
+GetGroupVersion = Action('GetGroupVersion')
+GetLoggerDefinition = Action('GetLoggerDefinition')
+GetLoggerDefinitionVersion = Action('GetLoggerDefinitionVersion')
+GetServiceRoleForAccount = Action('GetServiceRoleForAccount')
+GetSubscriptionDefinition = Action('GetSubscriptionDefinition')
+GetSubscriptionDefinitionVersion = \
+    Action('GetSubscriptionDefinitionVersion')
+ListCoreDefinitionVersions = Action('ListCoreDefinitionVersions')
+ListCoreDefinitions = Action('ListCoreDefinitions')
+ListDeployments = Action('ListDeployments')
+ListDeviceDefinitionVersions = Action('ListDeviceDefinitionVersions')
+ListDeviceDefinitions = Action('ListDeviceDefinitions')
+ListFunctionDefinitionVersions = Action('ListFunctionDefinitionVersions')
+ListFunctionDefinitions = Action('ListFunctionDefinitions')
+ListGroupCertificateAuthorities = \
+    Action('ListGroupCertificateAuthorities')
+ListGroupVersions = Action('ListGroupVersions')
+ListGroups = Action('ListGroups')
+ListLoggerDefinitionVersions = Action('ListLoggerDefinitionVersions')
+ListLoggerDefinitions = Action('ListLoggerDefinitions')
+ListSubscriptionDefinitionVersions = \
+    Action('ListSubscriptionDefinitionVersions')
+ListSubscriptionDefinitions = Action('ListSubscriptionDefinitions')
+ResetDeployments = Action('ResetDeployments')
+UpdateConnectivityInfo = Action('UpdateConnectivityInfo')
+UpdateCoreDefinition = Action('UpdateCoreDefinition')
+UpdateDeviceDefinition = Action('UpdateDeviceDefinition')
+UpdateFunctionDefinition = Action('UpdateFunctionDefinition')
+UpdateGroup = Action('UpdateGroup')
+UpdateGroupCertificateConfiguration = \
+    Action('UpdateGroupCertificateConfiguration')
+UpdateLoggerDefinition = Action('UpdateLoggerDefinition')
+UpdateSubscriptionDefinition = Action('UpdateSubscriptionDefinition')
