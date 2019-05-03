05f108cc3ab1eabc1612f83a221ff3c8a14bcaad
Michael Käufl <michael-k@users.noreply.github.com>
2019-05-03 17:59:15 +0200

    Support Python 3.6 and 3.7; discourage usage of 3.4

--- setup.py
+++ setup.py
@@ -12,4 +12,22 @@ setup(
     test_suite="tests",
     use_2to3=True,
     requires=['slimit'],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: BSD License",
+
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 2.7",
+    ],
 )
