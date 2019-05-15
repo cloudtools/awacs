d6c6df2eda6f7cb9b67fbb656baf2caa6b0a164d
Michael Käufl <michael-k@users.noreply.github.com>
2019-05-15 12:28:01 +0200

    Add new services Kinesis Analytics V2 and Managed Blockchain
    Action `GetApplicationState` (see `deleted_actions` in `gen.py`)
    appears twice in `kinesisanalytics.py`.  This should be fixed
    sometime, but is considered out of scope for this commit.
    
    ref https://github.com/awsdocs/iam-user-guide/commit/c5e488eb233107251328783ad529f51b2ae3897e

---
+++ awacs/managedblockchain.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2012-2013, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from aws import Action as BaseAction
+from aws import BaseARN
+
+service_name = 'Amazon Managed Blockchain'
+prefix = 'managedblockchain'
+
+
+class Action(BaseAction):
+    def __init__(self, action=None):
+        sup = super(Action, self)
+        sup.__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource='', region='', account=''):
+        sup = super(ARN, self)
+        sup.__init__(service=prefix, resource=resource, region=region,
+                     account=account)
+
+
+CreateMember = Action('CreateMember')
+CreateNetwork = Action('CreateNetwork')
+CreateNode = Action('CreateNode')
+CreateProposal = Action('CreateProposal')
+DeleteMember = Action('DeleteMember')
+DeleteNode = Action('DeleteNode')
+GetMember = Action('GetMember')
+GetNetwork = Action('GetNetwork')
+GetNode = Action('GetNode')
+GetProposal = Action('GetProposal')
+ListInvitations = Action('ListInvitations')
+ListMembers = Action('ListMembers')
+ListNetworks = Action('ListNetworks')
+ListNodes = Action('ListNodes')
+ListProposalVotes = Action('ListProposalVotes')
+ListProposals = Action('ListProposals')
+RejectInvitation = Action('RejectInvitation')
+VoteOnProposal = Action('VoteOnProposal')
