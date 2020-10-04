9b8e56a8819404eab42479dfceedcec36079bd7a
Michael Käufl <michael-k@users.noreply.github.com>
2020-10-04 18:19:53 +0200

    Drop support for Python 3.5
    It has reached its EOL in September 2020, see
    https://www.python.org/dev/peps/pep-0478/#release-schedule

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     test_suite="tests",
     use_2to3=True,
     requires=['slimit'],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    python_requires=(
+        ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
+    ),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -28,7 +30,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 2.7",
     ],
 )
