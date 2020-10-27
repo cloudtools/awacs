3954d6b3e4d25464e216353b1007ffb773ef03b3
Michael Käufl <michael-k@users.noreply.github.com>
2020-10-27 08:47:02 +0100

    Run tests against Python 3.9

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Operating System :: POSIX :: Linux",
 
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.6",
