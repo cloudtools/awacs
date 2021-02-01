9b9140a645219a4a9f606f97f19893d69bdc8494
GitHub Workflow <cloudtools@users.noreply.github.com>
2021-02-01 05:23:11 +0000

    Action Update 2021-05

---
+++ awacs/app_integrations.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012-2013, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from aws import Action as BaseAction
+from aws import BaseARN
+
+service_name = 'Amazon AppIntegrations'
+prefix = 'app-integrations'
+
+
+class Action(BaseAction):
+    def __init__(self, action=None):
+        sup = super(Action, self)
+        sup.__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource='', region='', account=''):
+        sup = super(ARN, self)
+        sup.__init__(service=prefix, resource=resource, region=region,
+                     account=account)
+
+
+CreateEventIntegration = Action('CreateEventIntegration')
+CreateEventIntegrationAssociation = \
+    Action('CreateEventIntegrationAssociation')
+DeleteEventIntegration = Action('DeleteEventIntegration')
+DeleteEventIntegrationAssociation = \
+    Action('DeleteEventIntegrationAssociation')
+GetEventIntegration = Action('GetEventIntegration')
+ListEventIntegrationAssociations = \
+    Action('ListEventIntegrationAssociations')
+ListEventIntegrations = Action('ListEventIntegrations')
+ListTagsForResource = Action('ListTagsForResource')
+TagResource = Action('TagResource')
+UntagResource = Action('UntagResource')
+UpdateEventIntegration = Action('UpdateEventIntegration')
