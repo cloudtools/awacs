bd8e319953c7133b9c334ef796c7c97a3a78e3ba
GitHub Workflow <cloudtools@users.noreply.github.com>
2021-11-29 05:14:58 +0000

    Action Update 2021-48

---
+++ awacs/backup_gateway.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "AWS Backup Gateway"
+prefix = "backup-gateway"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+AssociateGatewayToServer = Action("AssociateGatewayToServer")
+Backup = Action("Backup")
+CreateGateway = Action("CreateGateway")
+DeleteGateway = Action("DeleteGateway")
+DeleteHypervisor = Action("DeleteHypervisor")
+DisassociateGatewayFromServer = Action("DisassociateGatewayFromServer")
+ImportHypervisorConfiguration = Action("ImportHypervisorConfiguration")
+ListGateways = Action("ListGateways")
+ListHypervisors = Action("ListHypervisors")
+ListTagsForResource = Action("ListTagsForResource")
+ListVirtualMachines = Action("ListVirtualMachines")
+PutMaintenanceStartTime = Action("PutMaintenanceStartTime")
+Restore = Action("Restore")
+TagResource = Action("TagResource")
+TestHypervisorConfiguration = Action("TestHypervisorConfiguration")
+UntagResource = Action("UntagResource")
+UpdateGatewayInformation = Action("UpdateGatewayInformation")
+UpdateHypervisor = Action("UpdateHypervisor")
---
+++ awacs/ssm_guiconnect.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "AWS Systems Manager GUI Connect"
+prefix = "ssm-guiconnect"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+CancelConnection = Action("CancelConnection")
+GetConnection = Action("GetConnection")
+StartConnection = Action("StartConnection")
