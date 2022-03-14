4af8b98cb3a0fead837bffb62fb17c486dd48b2e
GitHub Workflow <cloudtools@users.noreply.github.com>
2022-03-14 05:36:08 +0000

    Action Update 2022-11

---
+++ awacs/rhelkb.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "Amazon RHEL Knowledgebase Portal"
+prefix = "rhelkb"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+GetRhelURL = Action("GetRhelURL")
