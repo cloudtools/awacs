16ebcd35b816315d5576e97a1df2ee8d82de4214
GitHub Workflow <cloudtools@users.noreply.github.com>
2022-07-04 05:28:32 +0000

    Action Update 2022-27

---
+++ awacs/sagemaker_groundtruth_synthetic.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "Amazon SageMaker Ground Truth Synthetic"
+prefix = "sagemaker-groundtruth-synthetic"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+CreateProject = Action("CreateProject")
+DeleteProject = Action("DeleteProject")
+GetBatch = Action("GetBatch")
+GetProject = Action("GetProject")
+ListBatchSummaries = Action("ListBatchSummaries")
+ListProjectSummaries = Action("ListProjectSummaries")
+UpdateBatch = Action("UpdateBatch")
