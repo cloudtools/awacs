f7d5a7472689beb55cbc4f5be6c9382693b1f6b7
GitHub Workflow <cloudtools@users.noreply.github.com>
2022-07-11 05:24:46 +0000

    Action Update 2022-28

---
+++ awacs/redshift_serverless.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "Amazon Redshift Serverless"
+prefix = "redshift-serverless"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+ConvertRecoveryPointToSnapshot = Action("ConvertRecoveryPointToSnapshot")
+CreateEndpointAccess = Action("CreateEndpointAccess")
+CreateNamespace = Action("CreateNamespace")
+CreateSnapshot = Action("CreateSnapshot")
+CreateUsageLimit = Action("CreateUsageLimit")
+CreateWorkgroup = Action("CreateWorkgroup")
+DeleteEndpointAccess = Action("DeleteEndpointAccess")
+DeleteNamespace = Action("DeleteNamespace")
+DeleteResourcePolicy = Action("DeleteResourcePolicy")
+DeleteSnapshot = Action("DeleteSnapshot")
+DeleteUsageLimit = Action("DeleteUsageLimit")
+DeleteWorkgroup = Action("DeleteWorkgroup")
+GetCredentials = Action("GetCredentials")
+GetEndpointAccess = Action("GetEndpointAccess")
+GetNamespace = Action("GetNamespace")
+GetRecoveryPoint = Action("GetRecoveryPoint")
+GetResourcePolicy = Action("GetResourcePolicy")
+GetSnapshot = Action("GetSnapshot")
+GetUsageLimit = Action("GetUsageLimit")
+GetWorkgroup = Action("GetWorkgroup")
+ListEndpointAccess = Action("ListEndpointAccess")
+ListNamespaces = Action("ListNamespaces")
+ListRecoveryPoints = Action("ListRecoveryPoints")
+ListSnapshots = Action("ListSnapshots")
+ListTagsForResource = Action("ListTagsForResource")
+ListUsageLimits = Action("ListUsageLimits")
+ListWorkgroups = Action("ListWorkgroups")
+PutResourcePolicy = Action("PutResourcePolicy")
+RestoreFromRecoveryPoint = Action("RestoreFromRecoveryPoint")
+RestoreFromSnapshot = Action("RestoreFromSnapshot")
+TagResource = Action("TagResource")
+UntagResource = Action("UntagResource")
+UpdateEndpointAccess = Action("UpdateEndpointAccess")
+UpdateNamespace = Action("UpdateNamespace")
+UpdateSnapshot = Action("UpdateSnapshot")
+UpdateUsageLimit = Action("UpdateUsageLimit")
+UpdateWorkgroup = Action("UpdateWorkgroup")
---
+++ awacs/rolesanywhere.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "AWS Identity and Access Management Roles Anywhere"
+prefix = "rolesanywhere"
+
+
+class Action(BaseAction):
+    def __init__(self, action: str = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+CreateProfile = Action("CreateProfile")
+CreateTrustAnchor = Action("CreateTrustAnchor")
+DeleteCrl = Action("DeleteCrl")
+DeleteProfile = Action("DeleteProfile")
+DeleteTrustAnchor = Action("DeleteTrustAnchor")
+DisableCrl = Action("DisableCrl")
+DisableProfile = Action("DisableProfile")
+DisableTrustAnchor = Action("DisableTrustAnchor")
+EnableCrl = Action("EnableCrl")
+EnableProfile = Action("EnableProfile")
+EnableTrustAnchor = Action("EnableTrustAnchor")
+GetCrl = Action("GetCrl")
+GetProfile = Action("GetProfile")
+GetSubject = Action("GetSubject")
+GetTrustAnchor = Action("GetTrustAnchor")
+ImportCrl = Action("ImportCrl")
+ListCrls = Action("ListCrls")
+ListProfiles = Action("ListProfiles")
+ListSubjects = Action("ListSubjects")
+ListTagsForResource = Action("ListTagsForResource")
+ListTrustAnchors = Action("ListTrustAnchors")
+TagResource = Action("TagResource")
+UntagResource = Action("UntagResource")
+UpdateCrl = Action("UpdateCrl")
+UpdateProfile = Action("UpdateProfile")
+UpdateTrustAnchor = Action("UpdateTrustAnchor")
