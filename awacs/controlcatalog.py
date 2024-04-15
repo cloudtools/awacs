d2f1559cab91ea5d8be5873dd5e3f75eec885eb0
GitHub Workflow <cloudtools@users.noreply.github.com>
2024-04-15 05:12:32 +0000

    Action Update 2024-16

---
+++ awacs/controlcatalog.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from typing import Optional
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "AWS Control Catalog"
+prefix = "controlcatalog"
+
+
+class Action(BaseAction):
+    def __init__(self, action: Optional[str] = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+ListCommonControls = Action("ListCommonControls")
+ListDomains = Action("ListDomains")
+ListObjectives = Action("ListObjectives")
