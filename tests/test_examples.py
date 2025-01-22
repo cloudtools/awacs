eb68efa39acdfdb740d8ca4060f66086fc6a33a3
Mark Peek <mark@peek.org>
2025-01-22 08:01:47 -0800

    Switch to pytest and other version updates
    - Switch to using pytest
    - Update python versions
    - Update dependency versions

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,13 @@
+import glob
 import os
-import re
+import pytest
 import sys
-import unittest
-from functools import partial
 
 
-class TestExamples(unittest.TestCase):
-    pass
+files = glob.glob(os.path.join("examples", "*.py"))
 
 
+@pytest.mark.parametrize("filename", files)
 def test_file(filename):
     # Ignore the output
     saved = sys.stdout
@@ -20,24 +19,3 @@ def test_file(filename):
                 exec(code)
         finally:
             sys.stdout = saved
-
-
-def add_tests():
-    # Filter out all *.py files from the examples directory
-    examples = "examples"
-    regex = re.compile(r".py$", re.I)
-    example_filesnames = filter(regex.search, os.listdir(examples))
-
-    # Add new test functions to the TestExamples class
-    for f in example_filesnames:
-        testname = "test_" + f[:-3]
-        testfunc = partial(test_file, examples + "/" + f)
-        # Get rid of partial() __doc__
-        testfunc.__doc__ = None
-        setattr(TestExamples, testname, testfunc)
-
-
-add_tests()
-
-if __name__ == "__main__":
-    unittest.main()
