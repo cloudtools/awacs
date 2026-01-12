bdf79d7c380358f9dae8580ae3f8c6bce2795f24
GitHub Workflow <cloudtools@users.noreply.github.com>
2026-01-12 05:35:52 +0000

    Action Update 2026-03

---
+++ awacs/ecs_mcp.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from typing import Optional
+
+from .aws import Action as BaseAction
+from .aws import BaseARN
+
+service_name = "Amazon ECS MCP Service"
+prefix = "ecs-mcp"
+
+
+class Action(BaseAction):
+    def __init__(self, action: Optional[str] = None) -> None:
+        super().__init__(prefix, action)
+
+
+class ARN(BaseARN):
+    def __init__(self, resource: str = "", region: str = "", account: str = "") -> None:
+        super().__init__(
+            service=prefix, resource=resource, region=region, account=account
+        )
+
+
+InvokeReadOnlyTools = Action("InvokeReadOnlyTools")
+UseMcp = Action("UseMcp")
